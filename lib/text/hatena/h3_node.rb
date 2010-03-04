a162b2325cf72ba284b9d5efb1e5a41531c388f8
Yasuhisa Yoshida <syou6162@gmail.com>
2010-03-04 13:33:20 +0900

    正規表現の文字コード回りを修正。あとカテゴリのリンク形式っぽくなるようにした

--- lib/text/hatena/h3_node.rb
+++ lib/text/hatena/h3_node.rb
@@ -19,16 +19,17 @@ module Text
         sa = c.sectionanchor
 
         if cat
-          cat.gsub!(/\[([^\:\[\]]+)\]/e) do
+          cat.gsub!(/\[([^\:\[\]]+)\]/u) do
+            puts $1
             w = $1
             ew = _encode($1)
-            %Q![<a href="#{b}?word=#{ew}" class="sectioncategory">#{w}</a>]!
+            %Q![<a href="#{b}?word=[#{ew}]" class="sectioncategory">#{w}</a>]!
           end
         end
         name, extra = _formatname(name)
         name ||= ""
         cat ||= ""
-        c.htmllines(%Q!#{t}<h3><a href="#{p}\##{name}" name="#{name}"><span class="sanchor">#{sa}</span></a> #{cat}#{title}</h3>#{extra}!)
+        c.htmllines(%Q!#{t}<h3><a href="#{p}" name="#{name}"><span class="sanchor">#{sa}</span></a> #{cat}#{title}</h3>#{extra}!)
       end
 
       def _formatname(name)
@@ -48,8 +49,8 @@ module Text
 
       def _encode(str)
         return nil if str.nil? or str.empty?
-        str.gsub!(/(\W)/e) do
-          sprintf("%%%02x", $1[0])
+        str.gsub!(/(\W)/u) do
+          sprintf("%%%02x", $1[0].ord)
         end
         return str
       end
